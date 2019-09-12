fb82fe85beb81c0c5a6fdd4a255d37e8a29972ee
Angel <angelglezz.7@gmail.com>
2019-09-12 17:42:27 -0500

    nfl part

--- js/indepth.js
+++ js/indepth.js
@@ -3,4 +3,22 @@ $(document).ready(function(){
         'centerOnScroll': true,
         'padding': 0
     });
+
+    $("#mx").click(function(){
+    	$("#liga").show();
+    	$("#main-content").hide();
+    	$(".close").show();
+    });
+
+    $("#usa").click(function(){
+    	$("#nfl").show();
+    	$("#main-content").hide();
+    	$(".close").show();
+    });
+
+    $(".close").click(function(){
+    	$(".indepth_content").hide();
+    	$("#main-content").show();
+    	$(".close").hide();
+    });
 });
